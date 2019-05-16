950dd2c5470f2fa83ef3791601232f4a8fa5c8b2
GeekHarp <ngbhandy@gmail.com>
2019-05-16 09:23:49 +0800

    Add feature added..

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 class List extends React.Component {
   constructor(){
     super()
-    this.changeHandler = this.changeHandler.bind( this );
+    //What does the following do?
+    this.changeHandler = this.changeHandler.bind(this);
+    // this.state = {word : ""}
   }
 
   state = {
@@ -9,26 +11,54 @@ class List extends React.Component {
     word : ""
   }
 
+  //This func() runs when the text<input> 'changes'
   changeHandler(event){
-    this.setState({word:event.target.value});
-    console.log("change", event.target.value);
+    // console.log(this)
+    this.setState({word : event.target.value});
+    // console.log(event.target.value);
+  }
+
+  clickHandler = () => {
+    let word = this.state.word;
+    //updatedList is 'current' state's []
+    let updatedList = this.state.list;
+    let newKey = updatedList.length;
+    console.log(updatedList);
+    updatedList.push(<ListItem key={newKey} item={this.state.word} />);
+    this.setState({
+        list: updatedList
+    });
   }
 
   render() {
       // render the list with a map() here
-
-      console.log("rendering");
+      let listLis = this.state.list;
+      console.log('rendering', listLis)
       return (
         <div className="list">
-          <input onChange={this.changeHandler} value={this.state.word}/>
-          <button>add item</button>
+          <input
+          onChange={this.changeHandler}
+          value={this.state.word}
+          />
+          <button
+          onClick={this.clickHandler}
+          >add item</button>
+          <ul>
+            {listLis}
+          </ul>
         </div>
       );
   }
+}
+
+class ListItem extends React.Component {
+    render() {
+        let todo = this.props.item;
+        return <li>{todo}</li>;
+    }
 }
 
 ReactDOM.render(
     <List/>,
     document.getElementById('root')
 );
-
